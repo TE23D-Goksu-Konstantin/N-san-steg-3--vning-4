ba3a2a66db229b32b3b8e2a59a4e3e0139ec5030
Konstantin Göksu <konstantin.goksu@elev.ga.ntig.se>
2024-11-22 10:15:10 +0100

    klar

---
+++ Näsan steg 3 övning 4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Triangeldrama
+{
+    
+    static int RightTriangleArea(int a, int b)
+    {
+        return((a*b)/2);  //Blir 10 eftersom svaret på a*b blir 21 då 10,5 ej går så den avrundar neråt
+    }
+
+    static void Main(string[] args){
+        Console.WriteLine(RightTriangleArea(3,7));
+        Console.ReadLine();
+    }
+
+
+
+}
